101f686e336bd3aceaf4b7f42f2182e35a8cb264
reactvehiclerentalsystem <kumaraswamibatta123@gmail.com>
2022-01-29 16:22:38 +0530

    first commit

---
+++ src/main/java/com/example/controller/DevopsController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DevopsController {
+	
+	@GetMapping("/")
+	public String hello() {
+		System.out.println("Hello DevOps");
+		return "Welcome DevOps";
+	}
+
+}
